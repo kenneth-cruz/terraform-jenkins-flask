bc3a28afa291d70acc448baa459b77dc2c41148e
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-03-01 17:34:27 -0500

    tf file back to original

--- kube.tf
+++ kube.tf
@@ -5,71 +5,78 @@ terraform {
     }
   }
 }
-
 provider "kubernetes" {
   config_path = "~/../../.kube/config"
 }
 
-resource "kubernetes_deployment" "jenkins" {
+resource "kubernetes_namespace" "flaskapp" {
   metadata {
-    name = "jenkins"
+    annotations = {
+      name = "flaskapp"
+    }
+
     labels = {
       App = "ScalableNginxExample"
     }
-  }
 
+    name = "flaskapp"
+  }
+}
+
+
+resource "kubernetes_deployment" "flaskapp" {
+  metadata {
+    namespace = kubernetes_namespace.flaskapp.metadata.0.name
+    name      = "flaskapp"
+    labels = {
+      App = "flaskapp"
+    }
+  }
   spec {
-    replicas = 1
+    replicas = 3
     selector {
       match_labels = {
-        App = "ScalableNginxExample"
+        App = "flaskapp"
       }
     }
     template {
       metadata {
         labels = {
-          App = "ScalableNginxExample"
+          App = "flaskapp"
         }
       }
       spec {
         container {
-          image = "jenkinsci/blueocean"
+          image = "tomkugelman/capstone-flask:latest"
           name  = "example"
-
           port {
-            container_port = 8080
+            container_port = 80
           }
 
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
         }
       }
     }
   }
+  timeouts {
+    create = "1m"
+    update = "1m"
+    delete = "2m"
+  }
 }
-
-resource "kubernetes_service" "jenkins_service" {
+resource "kubernetes_service" "flaskapp" {
   metadata {
-    name = "jenkins-service"
+    namespace = kubernetes_namespace.flaskapp.metadata.0.name
+    name      = "flaskapp"
   }
   spec {
     selector = {
-      App = kubernetes_deployment.jenkins.spec.0.template.0.metadata[0].labels.App
+      App = kubernetes_deployment.flaskapp.spec.0.template.0.metadata[0].labels.App
     }
     port {
       node_port   = 30201
-      port        = 8080
-      target_port = 8080
+      port        = 80
+      target_port = 80
     }
-
     type = "NodePort"
   }
-}
+} 
