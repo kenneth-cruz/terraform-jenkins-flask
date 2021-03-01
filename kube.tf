d1a8180d5831407eba46d44271f3140d6c127085
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-03-01 18:12:50 -0500

    kube edit

--- kube.tf
+++ kube.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source = 'hashicorp/kubernetes'
     }
   }
 }
 provider "kubernetes" {
-  config_path = '~/../../.kube/config'
+  config_path = "~/../../.kube/config"
 }
 
 resource "kubernetes_namespace" "flaskapp" {
